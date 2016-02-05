86ff3ca6d8986be4fe57816dd0cb0c677bee3066
Christopher Eppstein <ceppstein@linkedin.com>
2016-02-04 17:40:48 -0800

    Initial Version of the broccoli plugin for the css lang optimizer.

---
+++ README.md
@@ -0,0 +1,45 @@
+# broccoli-css-lang-optimizer
+
+This addon causes language specific rulesets in CSS to be extracted into
+per-language css files. The original CSS file has the language specific
+rulesets removed from it.
+
+This plugin uses postcss-lang-optimizer and adapts it for use in
+Broccoli projects.
+
+## API
+
+In your Brocfile.js:
+
+```js
+var LangOptimizer = require("broccoli-css-lang-optimizer");
+var inputNodes = ["static/css"]; // or wherever you put your CSS files.
+var options = { }; // Options for the plugin. See below.
+module.exports = new LangOptimizer(inputNodes, options);
+```
+
+## Options
+
+You can pass options to this plugin to control its behavior.
+
+```js
+{
+  // includeBaseFile controls whether the base css is included
+  // in every language-specific output file. When set to `false`,
+  // only the lang-specific selectors are output to the language
+  // specific file. Defaults to `true`.
+  includeBaseFile: true,
+  // filenameForLang is optional, when omited the filename is "<basename>_<lang>.css"
+  filenameForLang: function(baseFilename, lang) {
+    return baseFilename.replace(".css", "-" + lang + ".css");
+  },
+  // Passed along to the underlying postcss-lang-optimizer plugin.  Defaults to false.
+  subtags: false,
+  // Ensures a file is written for each of these languages (and only these languages)
+  // even if they are or are not specified in the source CSS.
+  // If omitted, languages are discovered from the source CSS file.
+  // This property can also be set to a function that returns an array of langs.
+  langs: ["en", "de", "zh"]
+}
+```
+
---
+++ test/test_langOptimizer.js
@@ -0,0 +1,133 @@
+/* Copyright 2016 LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ */
+
+"use strict";
+
+var assert = require("assert");
+var path = require("path");
+var fs = require("fs");
+var broccoli = require("broccoli");
+var RSVP = require("rsvp");
+var glob = require("glob");
+
+RSVP.on("error", function(reason, label) {
+  if (label) {
+    console.error(label);
+  }
+
+  console.assert(false, reason.message);
+});
+
+
+var LangOptimizer = require("../lib/langOptimizer");
+
+function fixtureSourceDir(name) {
+  return path.resolve(__dirname, "fixtures", name, "input");
+}
+
+function fixtureOutputDir(name) {
+  return path.resolve(__dirname, "fixtures", name, "output");
+}
+
+
+function build(builder) {
+  return RSVP.Promise.resolve()
+  .then(function() {
+    return builder.build();
+  })
+  .then(function(hash) {
+    return builder.tree.outputPath;
+  });
+}
+
+function diffDirs(actualDir, expectedDir, callback) {
+  var actualFiles = glob.sync("**/*", {cwd: actualDir}).sort();
+  var expectedFiles = glob.sync("**/*", {cwd: expectedDir}).sort();
+  assert.deepEqual(actualFiles, expectedFiles);
+  actualFiles.forEach(function(file) {
+    var actualPath = path.join(actualDir, file);
+    var expectedPath = path.join(expectedDir, file);
+    var stats = fs.statSync(actualPath);
+    if (stats.isFile()) {
+      assert.equal(fs.readFileSync(actualPath).toString(),
+                   fs.readFileSync(expectedPath).toString());
+    }
+  });
+  callback();
+}
+
+
+describe("LangOptimizer", function () {
+  it("can be instantiated", function (done) {
+    var optimizer = new LangOptimizer(fixtureSourceDir("basicProject"));
+    assert(optimizer instanceof LangOptimizer);
+    done();
+  });
+
+  it("can be instantiated by function call", function (done) {
+    var optimizer = LangOptimizer(fixtureSourceDir("basicProject"));
+    assert(optimizer instanceof LangOptimizer);
+    done();
+  });
+
+  it("splits css up by languages", function (done) {
+    var optimizer = LangOptimizer(fixtureSourceDir("basicProject"), {includeBaseFile: false});
+    var builder = new broccoli.Builder(optimizer);
+
+    build(builder)
+      .then(function(outputDir) {
+        diffDirs(outputDir, fixtureOutputDir("basicProject"), function() {
+          done();
+        });
+      });
+  });
+
+  it("includes the base file by default", function (done) {
+    var optimizer = LangOptimizer(fixtureSourceDir("includesBaseFile"));
+    var builder = new broccoli.Builder(optimizer);
+
+    build(builder)
+      .then(function(outputDir) {
+        diffDirs(outputDir, fixtureOutputDir("includesBaseFile"), function() {
+          done();
+        });
+      });
+  });
+
+  it("allows custom filenames", function (done) {
+    var optimizer = LangOptimizer(fixtureSourceDir("customFilenames"), {
+      includeBaseFile: false,
+      filenameForLang: function(baseFilename, lang) {
+         return baseFilename.replace(".css", "-" + lang + ".css");
+      }
+    });
+    var builder = new broccoli.Builder(optimizer);
+
+    build(builder)
+      .then(function(outputDir) {
+        diffDirs(outputDir, fixtureOutputDir("customFilenames"), function() {
+          done();
+        });
+      });
+  });
+
+  it("allows specified langs", function (done) {
+    var optimizer = LangOptimizer(fixtureSourceDir("specifiedLangs"), {
+      langs: ["en", "zh"]
+    });
+    var builder = new broccoli.Builder(optimizer);
+
+    build(builder)
+      .then(function(outputDir) {
+        diffDirs(outputDir, fixtureOutputDir("specifiedLangs"), function() {
+          done();
+        });
+      });
+  });
+});
