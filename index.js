85cf2d06ff9990dfabe717c249c3c14afcb7a2f6
Christopher Eppstein <ceppstein@linkedin.com>
2016-02-08 14:46:44 -0800

    Use the broccoli plugin.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /* jshint node: true */
 'use strict';
 
-var postCSSFilter = require("broccoli-postcss-sourcemaps");
-var langOptimizer = require("postcss-lang-optimizer");
+var LangOptimizer = require("broccoli-css-lang-optimizer");
 
 module.exports = {
   name: 'ember-cli-css-lang-optimizer',
@@ -13,39 +12,11 @@ module.exports = {
       name: 'lang-optimizer',
       ext: 'css',
       toTree: function(tree, inputPath, outputPath, options) {
-        var config = addon.project.config(addon.app.env).APP.langOptimizer || {};
-
-        return postCSSFilter(tree, {
-          plugins: [
-            {
-              module: langOptimizer,
-              options: {subtags: config.subtags}
-            }
-          ],
-          resultHandler: function(result, filter, relativePath, addOutputFile) {
-            var langResults = langOptimizer.extractAll(result);
-            var langs = config.langs ? config.langs : Object.keys(langResults)
-            if (typeof langs === "function") {
-              langs = langs();
-            }
-            langs.forEach(function (lang) {
-              var langFilename, langCSS;
-              if (config.filenameForLang) {
-                langFilename = config.filenameForLang(relativePath, lang);
-              } else {
-                langFilename = relativePath.replace(".css", "_" + lang + ".css")
-              }
-              if (config.includeBaseFile || typeof config.includeBaseFile == 'undefined') {
-                langCSS = result.css + "\n" + (langResults[lang] || "") + "\n";
-              } else {
-                langCSS = (langResults[lang] || "") + "\n";
-              }
-              addOutputFile(langCSS, langFilename);
-            });
-          }
-        });
+        var config = addon.project.config(addon.app.env).langOptimizer || {};
+        var tree = new LangOptimizer(tree, config); 
+        console.log(tree);
+        return tree;
       }
     });
   }
-
 };
